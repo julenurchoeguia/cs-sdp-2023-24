90c04de7e11353030432a3f6c7866fb7cd0975a8
Daniel Rodriguez <darc2203@gmail.com>
2024-02-09 23:58:10 +0100

    feat: Heuristic Model

--- python/models.py
+++ python/models.py
@@ -1,6 +1,8 @@
 import pickle
 from gurobipy import *
 from abc import abstractmethod
+from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
 
 import numpy as np
 
@@ -189,6 +191,7 @@ class TwoClustersMIP(BaseModel):
         """Instantiation of the MIP Variables - To be completed."""
         # Instanciation du modèle
         m = Model("First model")
+        m.setParam('TimeLimit', 5*60)
 
         # Constants
         self.e = 0.001 # Erreur de précision pour les inégalités strictes
@@ -251,7 +254,7 @@ class TwoClustersMIP(BaseModel):
                 # Containte valeur initiale nulle
                 self.model.addConstr(self.U[k][i][0] == 0)
             # Contraintes pour s'assurer que le max des utilités somment à 1
-            self.model.addConstr(sum([self.U[k][i][self.L-2] for i in range(self.n)]) == 1)
+            self.model.addConstr(sum([self.U[k][i][self.L] for i in range(self.n)]) == 1)
         
         
 
@@ -270,8 +273,10 @@ class TwoClustersMIP(BaseModel):
         i = 0
         while X[i] < x0:
             i += 1
-        assert i-1 >= 0, f" X = {X}, x0 = {x0}"
-        return Y[i-1] + (Y[i]-Y[i-1])/(X[i]-X[i-1])*(x0-X[i-1])
+        if i-1 >= 0:
+            return Y[i-1] + (Y[i]-Y[i-1])/(X[i]-X[i-1])*(x0-X[i-1])
+        else:
+            return Y[0]
 
     def U_k_i(self, k,i,x_j,U_coef):
         """Renvoie la valeur de la fonction U_k_i au point d'abscisse x"""
@@ -303,21 +308,36 @@ class TwoClustersMIP(BaseModel):
         return np.array(result)
 
 
-class HeuristicModel(BaseModel):
+class SingleClusterMIP(BaseModel):
     """Skeleton of MIP you have to write as the first exercise.
     You have to encapsulate your code within this class that will be called for evaluation.
     """
 
-    def __init__(self):
-        """Initialization of the Heuristic Model.
+    def __init__(self, n_pieces):
+        """Initialization of the MIP Variables
+
+        Parameters
+        ----------
+        n_pieces: int
+            Number of pieces for the utility function of each feature.
+        n_clusters: int
+            Number of clusters to implement in the MIP.
         """
         self.seed = 123
-        self.models = self.instantiate()
+        self.model = self.instantiate(n_pieces)
 
-    def instantiate(self):
-        """Instantiation of the MIP Variables"""
-        # To be completed
-        return
+    def instantiate(self, n_pieces):
+        """Instantiation of the MIP Variables - To be completed."""
+        # Instanciation du modèle
+        m = Model("First model")
+        m.setParam('TimeLimit', 5*60)
+
+        # Constants
+        self.e = 0.001 # Erreur de précision pour les inégalités strictes
+        self.L = n_pieces # Nombre de morceaux pour la fonction U
+
+        return m
+
 
     def fit(self, X, Y):
         """Estimation of the parameters - To be completed.
@@ -329,8 +349,79 @@ class HeuristicModel(BaseModel):
         Y: np.ndarray
             (n_samples, n_features) features of unchosen elements
         """
-        # To be completed
-        return
+        self.N = len(X) # Nombre de comparaisons
+        self.n = X.shape[1] # Nombre de features
+
+        # Instanciation des variables
+
+        # Coefficients de la fonction U
+        self.U_coeff = [[self.model.addVar(name=f"u_{i}_{l}") for l in range(self.L+1)] for i in range(self.n)] 
+        # Variables d'erreurs 
+        self.sigma_pos = [self.model.addVar( name=f"sigma_pos_{j}")  for j in range(self.N)]
+        self.sigma_neg = [self.model.addVar( name=f"sigma_neg_{j}")  for j in range(self.N)]
+
+        self.model.update()
+
+        # Calcul des abscisses des points de la fonction U
+        self.U_abscisse = [ l / self.L for l in range(self.L+1)]
+
+        # Ajout des contraintes
+        for j in range(self.N):
+            # Contraintes pour les variables binaires alpha
+            self.model.addConstr(self.U(X[j],self.U_coeff) - self.U(Y[j],self.U_coeff) + self.sigma_pos[j] - self.sigma_neg[j] >= 0)
+            # Contraintes pour les variables d'erreurs qui sont positives
+            self.model.addConstr(self.sigma_pos[j] >= 0)
+            self.model.addConstr(self.sigma_neg[j] >= 0)
+
+
+
+        for i in range(self.n):
+            for l in range(self.L):
+                # Contraintes monotonie
+                self.model.addConstr(self.U_coeff[i][l] <= self.U_coeff[i][l+1])
+            # Containte valeur initiale nulle
+            self.model.addConstr(self.U_coeff[i][0] == 0)
+        # Contraintes pour s'assurer que le max des utilités somment à 1
+        self.model.addConstr(sum([self.U_coeff[i][self.L] for i in range(self.n)]) == 1)
+        
+        
+
+        #Objectif
+        self.model.setObjective(sum([self.sigma_neg[j] + self.sigma_pos[j] for j in range(self.N)]), GRB.MINIMIZE)
+        self.model.optimize()
+        
+        print("Fit done !" + str(self.model.status) + " " + str(self.model.ObjVal))
+
+        self.U_sol = [[self.U_coeff[i][l].x for l in range(self.L+1)] for i in range(self.n)]
+
+    
+    def lineaire_morceaux(self, X,Y,x0):
+        """Renvoie l'ordonnée y0 d'un point d'abscisse x0 sur la courbe
+        passant par les points de coordonnées (X[i],Y[i])"""
+        i = 0
+        while X[i] < x0:
+            i += 1
+        if i-1 >= 0:
+            return Y[i-1] + (Y[i]-Y[i-1])/(X[i]-X[i-1])*(x0-X[i-1])
+        else:
+            return Y[0]
+
+    def U_i(self,i,x_j,U_coef):
+        """Renvoie la valeur de la fonction U_k_i au point d'abscisse x"""
+        try:
+            x = x_j[i]
+        except Exception as e:
+            print(x_j)
+            print(i)
+            print(len(x_j))
+            raise e
+        Y = [U_coef[i][l] for l in range(self.L+1)]
+        X = self.U_abscisse
+        return self.lineaire_morceaux(X,Y,x)
+
+    def U(self,x_j,U_coef):
+        """Renvoie la valeur de la fonction U au point d'abscisse x"""
+        return sum([self.U_i(i,x_j,U_coef) for i in range(self.n)])
 
     def predict_utility(self, X):
         """Return Decision Function of the MIP for X. - To be completed.
@@ -345,6 +436,76 @@ class HeuristicModel(BaseModel):
         np.ndarray:
             (n_samples, n_clusters) array of decision function value for each cluster.
         """
-        # To be completed
-        # Do not forget that this method is called in predict_preference (line 42) and therefor should return well-organized data for it to work.
+        result = []
+        for x in X:
+            result.append(self.U(x,self.U_sol))
+        return np.array(result)
+
+
+class HeuristicModel(BaseModel):
+    """Skeleton of MIP you have to write as the first exercise.
+    You have to encapsulate your code within this class that will be called for evaluation.
+    """
+
+    def __init__(self,n_clusters,clustering_method = "gmm"):
+        """Initialization of the Heuristic Model.
+        """
+        self.seed = 123
+        assert clustering_method in ["kmeans","gmm"], "Clustering method not recognized, please choose between 'kmeans' and 'gmm'"
+        self.clustering_method = clustering_method
+        self.n_clusters = n_clusters
+        self.instantiate()
+
+    def instantiate(self):
+        """Instantiation of the MIP Variables"""
+        if self.clustering_method == "kmeans":
+            self.clustering = KMeans(n_clusters=self.n_clusters, random_state=self.seed)
+        elif self.clustering_method == "gmm":
+            self.clustering = GaussianMixture(n_components=self.n_clusters, random_state=self.seed)
+        self.cluster_models = {i : SingleClusterMIP(n_pieces=5) for i in range(self.n_clusters)}
         return
+
+    def fit(self, X, Y):
+        """Estimation of the parameters - To be completed.
+
+        Parameters
+        ----------
+        X: np.ndarray
+            (n_samples, n_features) features of elements preferred to Y elements
+        Y: np.ndarray
+            (n_samples, n_features) features of unchosen elements
+        """
+        #V = np.concatenate([X,Y],axis=1)
+        V = X - Y
+        # Apply clustering
+        print("Fitting clustering")
+        self.clustering.fit(V)
+        # Save labels
+        self.labels = self.clustering.predict(V)
+
+        # Fit MIP Single Cluster model for each cluster
+        for i in range(self.n_clusters):
+            print(f"Fitting MIP to cluster {i}")
+            X_train = X[self.labels == i]
+            Y_train = Y[self.labels == i]
+            self.cluster_models[i].fit(X_train,Y_train)
+            print(f"Model {i} fitted")
+        
+
+    def predict_utility(self, X):
+        """Return Decision Function of the MIP for X. - To be completed.
+
+        Parameters:
+        -----------
+        X: np.ndarray
+            (n_samples, n_features) list of features of elements
+        
+        Returns
+        -------
+        np.ndarray:
+            (n_samples, n_clusters) array of decision function value for each cluster.
+        """
+        results = []
+        for i in range(self.n_clusters):
+            results.append(self.cluster_models[i].predict_utility(X).reshape(-1,1))
+        return np.concatenate(results,axis=1)
